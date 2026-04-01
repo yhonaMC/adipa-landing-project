f492850c056481901f7fefd513133ea36edd198e
yhonaiker moncada <yhonaiker1@gmail.com>
2026-04-01 18:45:08 -0500

    fix(version-b): fix asset loading on Railway deployment
    - Use relative paths for CSS/JS assets instead of asset() helper
    - Trust all proxies for Railway HTTPS reverse proxy

--- version-b/resources/views/layouts/app.blade.php
+++ version-b/resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/app.min.css') }}">
+    <link rel="stylesheet" href="/css/app.min.css">
 </head>
 <body>
     @include('sections.header')
@@ -26,6 +26,6 @@
 
     @include('sections.footer')
 
-    <script src="{{ asset('js/app.min.js') }}"></script>
+    <script src="/js/app.min.js"></script>
 </body>
 </html>
